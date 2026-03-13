935c57096eed037635f0a2ca640021ac4ede5a6b
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2026-03-14 00:50:46 +0500

    change port

--- server/utils/email.ts
+++ server/utils/email.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const config = useRuntimeConfig();
 const transporter = nodemailer.createTransport({
     host: config.emailHost,
-    port: Number(config.emailPort),
+    port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
         user: config.emailUser,
